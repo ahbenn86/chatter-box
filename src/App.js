5e87689faf5ced92fd23af7f688817bd1e4ef964
Angelo Bennett <77983302+ahbenn86@users.noreply.github.com>
2022-03-25 14:11:14 -0400

    created chat engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import {ChatEngine} from 'react-chat-engine';
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <h1>Welcome to Chatter Box</h1>
+      <ChatEngine
+        height="100vh"
+        projectID="4e40dd00-0495-44f2-86ae-0e7e4b6e7365"
+        userName="Angelo"
+        userSecret={process.env.REACT_APP_USER_PASSWORD}
+      />
     </div>
   );
 }
